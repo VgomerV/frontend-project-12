160d7ab28bc39f17b5e659d867809909057b04be
Gomer <vgomer@list.ru>
2024-12-30 01:24:13 +0300

    update index.html

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import init from './init.jsx';
 
+console.log('WORK');
+
 const app = async () => {
   const root = ReactDOM.createRoot(document.querySelector('#chat'));
   root.render(await init());

8ab1a867126985d11f3894a4b8acffb24cf408b3
Gomer <vgomer@list.ru>
2025-01-08 13:12:58 +0300

    update vite.config

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import init from './init.jsx';
 
-console.log('WORK');
-
 const app = async () => {
   const root = ReactDOM.createRoot(document.querySelector('#chat'));
   root.render(await init());

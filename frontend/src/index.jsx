3eca160336c2addde5c160c3159a6808ffd6666d
Gomer <vgomer@list.ru>
2025-01-08 20:26:16 +0300

    update package-lock.json

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,7 +4,7 @@ import init from './init.jsx';
 console.log("WORK-1");
 
 const app = async () => {
-  const root = ReactDOM.createRoot(document.querySelector('#chat'));
+  const root = ReactDOM.createRoot(document.getElementById('chat'));
   root.render(await init());
 };
 

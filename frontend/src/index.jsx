1f0e0cc9ae62d2568209b0505783dec65a0b32c3
Gomer <vgomer@list.ru>
2025-01-08 14:26:10 +0300

    test

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,9 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import init from './init.jsx';
 
+console.log("WORK-1");
+
 const app = async () => {
   const root = ReactDOM.createRoot(document.querySelector('#chat'));
   root.render(await init());
 };
 
 app();
+
+console.log("WORK-2");

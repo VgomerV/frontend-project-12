1938671c0ab800fc975d40e0607e57bc99c83c75
Gomer <vgomer@list.ru>
2025-01-16 23:21:09 +0300

    update LoginPage.jsa add validate login

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,12 +1,3 @@
-import { useNavigate } from 'react-router-dom';
-export const Home = () => {
-    const navigate = useNavigate();
-    const goHome = () => navigate('/login');
-
-    if (localStorage.length === 0) {
-        console.log('work');
-        goHome();
-    } else {
-        return <h1>ГЛАВНАЯ СТРАНИЦА</h1>;
-    }
-};
+import { Navigate } from 'react-router-dom';
+export const Home = () => localStorage.token ? <h1>ГЛАВНАЯ СТРАНИЦА</h1> : <Navigate to="/login" />;
+    
--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,61 +1,67 @@
 import axios from 'axios';
+import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import avatar from "../assets/avatar.jpg";
 import { useNavigate } from 'react-router-dom';
 
+const LogError = (isLogin) => isLogin ? <div className="invalid-tooltip">Неверные имя пользователя или пароль</div> : null;
+
 const SignupForm = () => {
-    const navigate = useNavigate();
-    const formik = useFormik({
-      initialValues: {
-        username: "",
-        password: "",
-      },
-      onSubmit: ({username, password}) => {
-        console.log(localStorage);
-        axios.post('api/v1/login', { username, password }).then((response) => {
-            const { token } = response.data;
-            // localStorage.removeItem('token');
-            localStorage.setItem('token', token);
-            if (localStorage.length > 0) {
-              console.log('WORK');
-              navigate('/');
-            }
-            console.log(localStorage.getItem('token'));
+  const [ logIn, setValid ] = useState(false);
+  const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit: ({username, password}) => {
+      axios.post('api/v1/login', { username, password })
+        .then((response) => {
+          const { token } = response.data;
+          localStorage.setItem('token', token);
+          if (localStorage.token) {
+            navigate('/');
+          }
+        })
+        .catch(() => {
+          setValid(true);
         });
       },
     });
-    return (
-      <form onSubmit={formik.handleSubmit} className="col-12 col-md-6 mt-3 mt-md-0">
-        <h1 className="text-center mb-4">Войти</h1>
-        <div className="form-floating mb-3">
-            <input
-            type="username"
-            name="username"
-            id="username"
-            className="form-control"
-            autocomplete="username"
-            required
-            placeholder="Ваш ник"
+
+  return (
+    <form onSubmit={formik.handleSubmit} className="col-12 col-md-6 mt-3 mt-md-0">
+      <h1 className="text-center mb-4">Войти</h1>
+      <div className="form-floating mb-3">
+          <input
+          type="username"
+          name="username"
+          id="username"
+          className={`form-control ${logIn ? 'is-invalid' : ''}`}
+          autocomplete="username"
+          required
+          placeholder="Ваш ник"
+          onChange={formik.handleChange}
+        value={formik.values.username}
+        />
+        <label htmlFor="username">Ваш ник</label>
+      </div>
+      <div className="form-floating mb-4">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className={`form-control ${logIn ? 'is-invalid' : ''}`}
+            placeholder="Пароль"
             onChange={formik.handleChange}
-            value={formik.values.username}
-            />
-            <label htmlFor="username">Ваш ник</label>
-        </div>
-        <div className="form-floating mb-4">
-            <input
-                type="password"
-                name="password"
-                id="password"
-                className="form-control"
-                placeholder="Пароль"
-                onChange={formik.handleChange}
-                value={formik.values.password}
-            />
-            <label htmlFor="password" className="form-label">Пароль</label>
-        </div>
-        <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
-      </form>
-    );
+            value={formik.values.password}
+          />
+          <label htmlFor="password" className="form-label">Пароль</label>
+          {LogError(logIn)}
+      </div>
+      <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
+    </form>
+  );
 };
 
 export const Login = () => (

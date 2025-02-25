9d51e9541e8166c9ee28f44c7de10b141c31dc47
Vitaly Gomer <vgomer@list.ru>
2025-02-25 21:32:49 +0300

    add rollbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
@@ -26,32 +27,40 @@ const App = () => {
     const ru = filter.getDictionary('ru');
     filter.add(ru);
 
+    const rollbarConfig = {
+      accessToken: process.env.TOKEN_ROLLBAR,
+      captureUncaught: true,
+      captureUnhandledRejections: true,
+    };
+
   return (
-    <I18nextProvider i18n={i18n}>
-      <Provider store={store}>
-        <Router future = {{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-          <ToastContainer
-              position="top-right"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick={false}
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="light"
-              transition={Bounce}
-            />
-          <Routes>
-            <Route path = '/' element={<MainPage />} />
-            <Route path = '/login' element={<LoginPage />} />
-            <Route path = '/signup' element={<SignUp />} />
-            <Route path = '*' element={<NotFoundPage />} />
-          </Routes>
-        </Router>
-      </Provider>
-    </I18nextProvider>
+    <RollbarProvider config={rollbarConfig}>
+      <I18nextProvider i18n={i18n}>
+        <Provider store={store}>
+          <Router future = {{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                transition={Bounce}
+              />
+            <Routes>
+              <Route path = '/' element={<MainPage />} />
+              <Route path = '/login' element={<LoginPage />} />
+              <Route path = '/signup' element={<SignUp />} />
+              <Route path = '*' element={<NotFoundPage />} />
+            </Routes>
+          </Router>
+        </Provider>
+      </I18nextProvider>
+    </RollbarProvider>
   );
 };
 
